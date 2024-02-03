4f678a6e8ad1cd18c1bb280f522db21d872a3953
Jonathan <jonathankbs@gmail.com>
2024-02-02 20:26:12 -0600

    Se crea estructura para meta description

--- _tpl/base.tpl.php
+++ _tpl/base.tpl.php
@@ -5,8 +5,8 @@
     <meta charset=utf-8>
     <meta http-equiv=X-UA-Compatible content="IE=edge">
     <meta name=viewport content="width=device-width, initial-scale=1">
-    <title>Agrogen</title>
-    <meta name="description" content="">
+    <title><?php echo $tpl["titulo"] ?></title>
+    <meta name="description" content="<?php echo $tpl["descripcion"] ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="<?php echo $_n ?>img/logos/favicon.png">
     <!-- ====== main style start ====== -->
