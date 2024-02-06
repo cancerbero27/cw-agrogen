e5beda39a5945759336d1cf67decffb8ba972290
Jonathan <jonathankbs@gmail.com>
2024-02-06 17:34:44 -0600

    Se crea php para pasar contenido a html

---
+++ pasarhtml.php
@@ -0,0 +1,28 @@
+<?php 
+    copy("http://localhost/agrogen/contacto.html",          "html/contacto.html");
+    copy("http://localhost/agrogen/directorio.html",        "html/directorio.html");
+    copy("http://localhost/agrogen/index.html",             "html/index.html");
+    copy("http://localhost/agrogen/nosotros.html",          "html/nosotros.html");
+    copy("http://localhost/agrogen/productos.html",         "html/productos.html");
+    copy("http://localhost/agrogen/recursos-tecnicos.html", "html/recursos-tecnicos.html");
+    
+    // Productos
+    copy("http://localhost/agrogen/productos/acido-sulfurico.html",         "html/productos/acido-sulfurico.html");
+    copy("http://localhost/agrogen/productos/agroxtend-gold.html",          "html/productos/agroxtend-gold.html");
+    copy("http://localhost/agrogen/productos/agroxtend-platinum.html",      "html/productos/agroxtend-platinum.html");
+    copy("http://localhost/agrogen/productos/cloruro-potasio.html",         "html/productos/cloruro-potasio.html");
+    copy("http://localhost/agrogen/productos/complejo-triple-16.html",      "html/productos/complejo-triple-16.html");
+    copy("http://localhost/agrogen/productos/dap.html",                     "html/productos/dap.html");
+    copy("http://localhost/agrogen/productos/fosfo-n34.html",               "html/productos/fosfo-n34.html");
+    copy("http://localhost/agrogen/productos/fosfo-n34max.html",            "html/productos/fosfo-n34max.html");
+    copy("http://localhost/agrogen/productos/fosfonitrato.html",            "html/productos/fosfonitrato.html");
+    copy("http://localhost/agrogen/productos/map.html",                     "html/productos/map.html");
+    copy("http://localhost/agrogen/productos/sulfamin45.html",              "html/productos/sulfamin45.html");
+    copy("http://localhost/agrogen/productos/sulfamin45-compactado.html",   "html/productos/sulfamin45-compactado.html");
+    copy("http://localhost/agrogen/productos/sulfamin45-granulado.html",    "html/productos/sulfamin45-granulado.html");
+    copy("http://localhost/agrogen/productos/sulfamin45-premium.html",      "html/productos/sulfamin45-premium.html");
+    copy("http://localhost/agrogen/productos/sulfamin45-ultrasoluble.html", "html/productos/sulfamin45-ultrasoluble.html");
+    copy("http://localhost/agrogen/productos/super-nitrato.html",           "html/productos/super-nitrato.html");
+    copy("http://localhost/agrogen/productos/urea-prilada.html",            "html/productos/urea-prilada.html");
+
+?>
