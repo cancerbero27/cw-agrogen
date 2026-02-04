273aa16899cd23aadc906bab6ab50d06f4afd0f2
Jonathan Molina <jonathankbs@gmail.com>
2026-02-04 10:41:06 -0600

    Actualización de contenido

--- _tpl/base.tpl.php
+++ _tpl/base.tpl.php
@@ -26,7 +26,6 @@
 <body>
     <div class="preloader text-center">
         <img src="<?php echo $_n ?>img/logos/preloader-logo.png" alt="">
-        <h1 class="preloader-texto f1">Más de 30 años sirviendo al campo Mexicano</h1>
         <div class="spinner"></div>
     </div>
     <!-- page-wrapper start -->
--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 function owlCarouselActivation() {
     if ($(".banner-carousel").length) {
         $(".banner-carousel").owlCarousel({
-            autoplay: false,
+            autoplay: true,
             autoplayTimeout: 8000,
             loop: true,
             animateOut: "fadeOut",
